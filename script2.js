a53b539497acf422cfca3e0f012a4b1f7eebc987
Keystone <keystonebarreto@gmail.com>
2024-05-07 15:06:44 -0300

    CRUD de categorias finalizado

--- script2.js
+++ script2.js
@@ -7,6 +7,9 @@ class Category {
 
 const categoryList = document.getElementById('category-list');
 const addCategoryForm = document.getElementById('add-category-form');
+const form_title = document.getElementById('form-title');
+const btn_add = document.getElementById('form-btn-add');
+const btn_edit = document.getElementById('form-btn-edit');
 
 document.addEventListener('DOMContentLoaded', listCategories);
 
@@ -85,6 +88,65 @@ function addCategory(categoryData) {
         });
 }
 
+function removeCategory(categoryId) {
+    return fetch(`https://web01-miniprojeto04-default-rtdb.firebaseio.com/categories/${categoryId}.json`, {
+        method: 'DELETE'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta de rede não foi ok');
+            }
+            listCategories();
+        });
+}
+
+function changeForm(categoryId) {
+    return fetch(`https://web01-miniprojeto04-default-rtdb.firebaseio.com/categories/${categoryId}.json`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta de rede não foi ok');
+            }
+            return response.json();
+        })
+        .then(category => {
+            console.log(`Categoria ${category.name} recuperada, dale!`);
+
+            const nameField = document.getElementById('name');
+            nameField.value = category.name;
+
+            btn_add.style.display = 'none';
+            btn_edit.removeAttribute('style');
+            form_title.textContent = "Editar Categoria"
+            btn_edit.setAttribute('onclick', `updateCategory('${categoryId}')`);
+        });
+}
+
+function updateCategory(categoryId) {
+    const formData = new FormData(addCategoryForm);
+    const categoryData = {
+        name: formData.get('name')
+    };
+
+    return fetch(`https://web01-miniprojeto04-default-rtdb.firebaseio.com/categories/${categoryId}.json`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(categoryData),
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Resposta de rede não foi ok');
+            }
+            btn_edit.style.display = 'none';
+            btn_add.removeAttribute('style');
+            form_title.textContent = "Adicionar Nova Categoria"
+
+            listCategories();
+            addCategoryForm.reset(); // Limpa os campos do formulário
+        });
+}
+
 // Função para renderizar categorias na página
 function renderCategories(categories) {
     categoryList.innerHTML = ''; // Limpa as categorias existentes
@@ -108,7 +170,28 @@ function createCategoryCard(category) {
     name.classList.add('card-title');
     name.textContent = category.name;
 
+    const btn_group = document.createElement('div');
+    btn_group.classList.add('btn-group');
+    btn_group.setAttribute('role', 'group');
+
+    const btn_remove = document.createElement('button');
+    btn_remove.classList.add('btn');
+    btn_remove.classList.add('btn-danger');
+    btn_remove.setAttribute('type', 'button');
+    btn_remove.setAttribute('onclick', `removeCategory('${category.id}')`);
+    btn_remove.innerHTML = "Remover";
+
+    const btn_update = document.createElement('button');
+    btn_update.classList.add('btn');
+    btn_update.classList.add('btn-primary');
+    btn_update.setAttribute('type', 'button');
+    btn_update.setAttribute('onclick', `changeForm('${category.id}')`);
+    btn_update.innerHTML = "Editar";
+
     categoryCard.appendChild(name);
+    btn_group.appendChild(btn_remove);
+    btn_group.appendChild(btn_update);
+    categoryCard.appendChild(btn_group);
 
     return categoryCard;
 }
